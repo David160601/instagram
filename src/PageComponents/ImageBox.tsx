bd52b21fe73a721b3f1de3d54a33e668579c8a51
david <phengdavid979@gmail.com>
2022-03-11 19:10:49 +0700

    general

--- src/PageComponents/ImageBox.tsx
+++ src/PageComponents/ImageBox.tsx
@@ -5,10 +5,17 @@ type Props = {};
 
 const ImageBox = (props: Props) => {
   return (
-    <Box
-      component="img"
-      src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png"
-    ></Box>
+    <Box component="div" sx={{ position: "relative" }}>
+      <Box
+        component="img"
+        src="https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png"
+      ></Box>
+      <Box
+        component="img"
+        src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png"
+        sx={{ position: "absolute", left: 153, top: 28 }}
+      ></Box>
+    </Box>
   );
 };
 
