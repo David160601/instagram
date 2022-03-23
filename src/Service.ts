4c20e16998352d3701083de8d3b552e7741655e6
david <phengdavid979@gmail.com>
2022-03-23 21:33:44 +0700

    general

--- src/Service.ts
+++ src/Service.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-const apiUrl = process.env.DEVELOPMENT_API_URL;
 const service = {
     post: async (url: string, data: any) => {
         return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, data)
