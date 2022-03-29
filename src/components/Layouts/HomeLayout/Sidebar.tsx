7b8ac14863b5c79260c6699bd1526771afe960ae
david <phengdavid979@gmail.com>
2022-03-29 15:07:12 +0700

    side bar

--- src/components/Layouts/HomeLayout/Sidebar.tsx
+++ src/components/Layouts/HomeLayout/Sidebar.tsx
@@ -5,30 +5,9 @@ function Sidebar() {
   return (
     <Box
       component="div"
-      sx={{ background: "yellow", position: "sticky", top: 0, left: 0 }}
+      sx={{ background: "yellow", position: "sticky", top: "10vh", left: 0 }}
     >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati
-      sed quidem eum, ipsa amet officia non quia totam odit mollitia. Expedita a
-      accusantium impedit tempore hic, quis ducimus velit debitis similique
-      reiciendis reprehenderit! Qui cumque maxime accusamus saepe veritatis quas
-      minima dolor blanditiis praesentium corporis, reprehenderit hic iusto
-      dolores aut eveniet voluptatum nobis magnam placeat, ab sit? Quidem dicta
-      incidunt commodi perspiciatis dolorem adipisci suscipit ratione inventore
-      magni assumenda. Molestias, odio at temporibus atque laudantium veritatis
-      assumenda consequuntur repellendus odit quis nemo natus suscipit, voluptas
-      ducimus in molestiae reiciendis quia dolor facilis non. At praesentium
-      consequuntur suscipit magni accusamus alias minima, aspernatur soluta
-      reiciendis voluptates provident sed consequatur quia autem aliquam odit!
-      Modi qui voluptates accusantium exercitationem laborum iusto numquam esse,
-      doloribus doloremque consequatur aperiam porro, sit animi provident
-      ratione placeat repudiandae! Rem architecto nihil perferendis saepe, ipsum
-      cumque cum eum beatae tempora molestias doloribus quis voluptas
-      accusantium hic unde quisquam inventore impedit quibusdam! Et debitis iste
-      deserunt, voluptates laborum eos explicabo quasi veritatis nulla
-      exercitationem iusto cumque hic dolorem fugiat ab esse incidunt itaque
-      quidem sunt ducimus laboriosam aliquam temporibus aspernatur repellat?
-      Nostrum dolorum reprehenderit natus? Praesentium alias repellat officia,
-      natus consequatur minima nulla delectus beatae perspiciatis inventore.
+      test
     </Box>
   );
 }
